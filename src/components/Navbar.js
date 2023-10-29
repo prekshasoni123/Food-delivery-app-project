73d356fee606113b4f5e743b09adbe3ea7ecfe03
Preksha Soni <divysoni2812@gmail.com>
2023-10-29 18:25:54 +0530

    new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const handleLogout=()=>{
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
   <div className="container-fluid">
-    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+    <Link className="navbar-brand fs-1 fst-italic" to="/">Food Munch</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
